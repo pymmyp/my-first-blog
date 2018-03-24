7b68d6695b05c3533c52e73096f95bc89da34fe2
pymmyp <pymmyp@hotmail.com>
2018-03-24 16:31:56 +0700

    V4

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,11 @@
 # Function render to render html
 from django.shortcuts import render
 from django.utils import timezone
+from django.shortcuts import redirect
 
 # Import from views
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 
@@ -11,3 +13,21 @@ def post_list(request):
     #Django query set
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    #Django query set
+    post = Post.objects.get(pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
