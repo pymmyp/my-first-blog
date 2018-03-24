5d9fb3409406a5b367c5ba6139cb5501e56e667f
pymmyp <pymmyp@hotmail.com>
2018-03-24 14:54:35 +0700

    Add CSS

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
 # Function render to render html
+from django.shortcuts import render
+from django.utils import timezone
+
+# Import from views
+from .models import Post
+
 # Create your views here.
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    #Django query set
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
